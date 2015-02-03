b6502fcdae071253b6556d313d5a95d339f19c63
John McCormack <mccormack.wgsi@gmail.com>
2015-02-03 18:44:29 -0500

    Discussion draft, need to add in input and output images

--- Homework1/ExtractCoins.py
+++ Homework1/ExtractCoins.py
@@ -1,5 +1,22 @@
 import Image
 
+
+##################################
+#
+# binarize
+#
+# This function converts the image into a two
+# color image. 
+#
+# Inputs: pix = the image
+#         width = the width of the image
+#         height = the height of the image
+#
+# Output: returns pix, the image, but with the circles
+#         filled in. 
+#
+###################################
+
 def  binarize (pix, width, height):
 	for x in range(width):
 		for y in range(height):
@@ -12,6 +29,31 @@ def  binarize (pix, width, height):
 		for y in range(height):
 			averagePixel(x,y,pix,width,height)
 	return pix
+    
+##################################
+#
+# expandCircle
+#
+# Once a circle is identified, this
+# function can be used to expand the circle.
+# It starts from the point and searches all 
+# its neighbours for valid parts of the circle.
+# It uses a breadth first search and appends possible
+# circle points to a list. 
+#
+# Inputs: x = x coordinate of pixel
+#         y = y coordinate of pixel
+#         pix = the image
+#         width = the width of the image
+#         height = the height of the image
+#         count = the number of circles found.
+#                 this number is used to increment
+#                 the grey scale.
+#
+# Output: returns pix, the image, but with a circle
+#         filled in. 
+#
+###################################
 
 def expandCircle(x,y,pix,width,height,count):
 	gray_level = 10 * count
@@ -21,34 +63,45 @@ def expandCircle(x,y,pix,width,height,count):
 	while(done == False):
 		if x + 1 < width:
 			if pix[x+1,y] == 255:
-				#print "Check1"
 				pix[x+1,y] = gray_level
 				search.append([x+1,y])
 		if x - 1 >= 0:
 			if pix[x-1,y] == 255:
-				#print "Check2"
 				pix[x-1,y] = gray_level
 				search.append([x-1,y])
 		if y + 1 < height:
 			if pix[x,y+1] == 255:
-				#print "Check3"
 				pix[x,y+1] = gray_level
 				search.append([x,y+1])
 		if y - 1 >= 0:
 			if pix[x,y-1] == 255:
-				#print "Check4"
 				pix[x,y-1] = gray_level
 				search.append([x,y-1])
 		if len(search) != 0:
 			next_point = search.pop()
-			#print search
 			y = next_point[1]
 			x = next_point[0]
 		else:
 			done = True
 	return pix
-	
+
+##################################
+#
+# findCircle
+#
+# This function is used to find circles
+#
+# Inputs: pix = the image
+#         width = the width of the image
+#         height = the height of the image
+#
+# Output: returns pix, the image, but with the circles
+#         filled in. 
+#
+###################################
+
 def findCircle(pix, width, height):
+#count is used to keep a total for incremnting the grey scale
 	count = 1
 	for x in range(width):
 		for y in range(height):
@@ -57,6 +110,23 @@ def findCircle(pix, width, height):
 				count += 1
 	return pix
 		
+        
+##################################
+#
+# averagePixel
+#
+# This function is used to smooth over 
+# stray noise areas.
+#
+# Inputs: x = x coordinate of pixel
+#         y = y coordinate of pixel
+#         pix = the image
+#         width = the width of the image
+#         height = the height of the image
+#
+# Output: returns pix, the image, but with the pixel averaged
+#
+###################################
 def averagePixel(x,y,pix,width,height):
 	sum = pix[x,y]
 	count = 1
@@ -100,7 +170,3 @@ if __name__ == '__main__':
 	pix = binarize(pix, width, height)
 	pix = findCircle(pix, width, height)
 	img.save('coins2.png')
-
-
-		
-	
--- Homework1/Invert.py
+++ Homework1/Invert.py
@@ -1,10 +1,20 @@
 import Image
 
-img = Image.open("coins.png")
-pix = img.load()
-(width, height) = img.size
-for x in range(width):
-	for y in range(height):
-		r,g,b = pix[x,y]
-		pix[x,y] = (255 - r, 255 - g, 255- b)
-img.save("coins2.png")
+def inverse_image():
+    #Open Image 
+    img = Image.open("coins.png")
+    #Load pixel information
+    pix = img.load()
+    #Find the width and height of the image
+    (width, height) = img.size
+    #Iterate over the width and the height
+    for x in range(width):
+        for y in range(height):
+            #Replace each pixel with the number referenced from 255 instead of 0
+            r,g,b = pix[x,y]
+            pix[x,y] = (255 - r, 255 - g, 255- b)
+    #Save Input
+    img.save("coins2.png")
+
+if __name__ == "__main__":
+    inverse_image()
