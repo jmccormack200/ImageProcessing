3e7bd18567445917721bb52895016e2d9bb47656
John McCormack <mccormack.wgsi@gmail.com>
2015-03-17 15:35:11 -0400

    fixed major issue but still not correct

--- Homework3/spatialfilters.py
+++ Homework3/spatialfilters.py
@@ -7,10 +7,35 @@ class ImageProcessing:
 
     def __init__(self, imagepath):
         self.image = Image.open(imagepath).convert("L")
-        (self.width, self.height) = self.image.size 
+        (self.width, self.height) = self.image.size
+        #pad image
+        self.__pad()
+        
+        
+        
+    def __pad(self):
+        pix = self.image.load()
+        padded_array = []
+        padded_array.append([0]*(self.width + 4))
+        padded_array.append([0]*(self.width + 4))
+        for y in range(self.height):
+            row = []
+            for x in range(self.width + 4):
+                if ((x <= 1) or (x - 2 >= self.width)):
+                    row.append(0)
+                else:
+                    row.append(pix[x-2,y])
+            padded_array.append(row)
+        padded_array.append([0]*(self.width + 4))
+        padded_array.append([0]*(self.width + 4))
+        im = Image.fromarray(np.uint8(padded_array))
+        self.image = im
+        (self.width, self.height) = self.image.size
+        
         
     def outputImageAs(self, filename):
-        self.image.convert("RGB").save(filename + ".bmp")
+        
+        self.image.save(filename + ".bmp")
         
     def linearSmoothingFilter(self, weighted=False):
         if weighted:
@@ -82,38 +107,32 @@ class ImageProcessing:
         new_image = self.image.copy()
         #pix = self.image.load()
         pix = new_image.load()
-        for x in range(self.width):
-            for y in range(self.height):
+        for x in range(2, self.width-2):
+            for y in range(2, self.height-2):
                 middle_value = 0
                 middle_value += pix[x,y] * filtergrid[1][1]
-                #print middle_value
-                if (y+1 < self.height):
-                    middle_value += pix[x,y+1] * filtergrid[1][2]
-                if (y-1 >= 0):
-                    middle_value += pix[x,y-1] * filtergrid[1][0]
-                if (x+1 < self.width):
-                    middle_value += pix[x+1,y] * filtergrid[2][1]
-                    if (y+1 < self.height):
-                        middle_value += pix[x+1,y+1] * filtergrid[2][2]
-                    if (y-1 >= 0):
-                        middle_value += pix[x+1,y-1] * filtergrid[2][0]
-                if (x-1 >= 0):
-                    middle_value += pix[x-1,y] * filtergrid[0][1]
-                    if (y+1 < self.height):
-                        middle_value += pix[x-1,y+1] * filtergrid[0][2]
-                    if (y-1 >= 0):
-                        middle_value += pix[x-1,y-1] * filtergrid[0][0]
-                #print middle_value
-                pix[x,y] = pix[x,y] + ((int(middle_value / constant)))
-        new_image.convert("RGB").save(name + ".bmp")
+                middle_value += pix[x,y+1] * filtergrid[1][2]
+                middle_value += pix[x,y-1] * filtergrid[1][0]
+                middle_value += pix[x+1,y] * filtergrid[2][1]
+                middle_value += pix[x+1,y+1] * filtergrid[2][2]
+                middle_value += pix[x+1,y-1] * filtergrid[2][0]
+                middle_value += pix[x-1,y] * filtergrid[0][1]
+                middle_value += pix[x-1,y+1] * filtergrid[0][2]
+                middle_value += pix[x-1,y-1] * filtergrid[0][0]
+                if middle_value < 0:
+                    middle_value *= -1
+                pix[x,y] = (int(middle_value / constant))
+        new_image.save(name + ".jpg")
 
 
 
         
 if __name__ == "__main__":
-    imageprocessing = ImageProcessing("shapes.jpg")
+    #imageprocessing = ImageProcessing("shapes.jpg")
+    #imageprocessing = ImageProcessing("Boston_Normal.bmp")
+    imageprocessing = ImageProcessing("wdg4.gif")
     #imageprocessing.outputImageAs("Test")
-    #imageprocessing.linearSmoothingFilter()
+    imageprocessing.linearSmoothingFilter()
     imageprocessing.linearSmoothingFilter(weighted=True)
     #imageprocessing.gaussianFilter(0.5)
     #imageprocessing.medianFilter()
