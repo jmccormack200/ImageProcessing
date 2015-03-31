a53075fca4eeef2a86cd7f28f02bbebee5343b5f
John McCormack <mccormack.wgsi@gmail.com>
2015-03-31 00:30:07 -0400

    not working but stopping for the night

--- Homework4/dft.py
+++ Homework4/dft.py
@@ -17,7 +17,9 @@ class DFT:
             for v in range(self.height):
                 complex = self.computePoint(u,v)
                 outputPower[v][u] = sqrt((complex.real)**2 + (complex.imag)**2).real
+                print complex
                 outputPhase[v][u] = atan(complex.imag/complex.real).real
+            print u
         
         outputDFT_im = Image.fromarray(outputPower)
         outputDFT_im.convert('RGB').save("dftPower.jpg")
@@ -26,13 +28,15 @@ class DFT:
         outputPoint = 0
         for x in range(self.width):
             for y in range(self.height):
-                outputPoint += self.pix[x][y] * exp(-1j*pi*
+                outputPoint += self.pix[y][x] * exp(-1j*pi*
                         (((u*x)/self.width) + ((v*y)/self.height)))
         return outputPoint       
 
 
 if __name__ == "__main__":
     imagePath = "skull.gif"
+    #imagePath = "cln1.gif"
+    #imagePath = "360e.png"
     dft = DFT(imagePath)
     dft.takeDft()
     
