3a0a800e92c2a6facb62f5220e653ac670d18086
John McCormack <mccormack.wgsi@gmail.com>
2015-04-08 21:45:54 -0400

    Start of Homework 5

---
+++ Homework5/fft_filter.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+class fft_filter():
+    
+    def __init__(self,imagePath):
+        
+        
+
+        
+if __name__ == "__main__":
+    fft_filter()
