d91dc4069b05232592575d2ceb624f09f107a1cf
John McCormack <mccormack.wgsi@gmail.com>
2015-04-11 12:48:45 -0400

    starting to work, need to fix edge cases for ILP

--- Homework5/fft_filter.py
+++ Homework5/fft_filter.py
@@ -30,19 +30,39 @@ class fft_filter():
         
     def __fft(self):
         self.fft = np.fft.fft2(self.paddedimage)
-        fshift = np.fft.fftshift(self.fft)
-        self.magnitudeSpec = 20*np.log(np.abs(fshift))
-        self.phaseSpec = 20*np.log(np.angle(fshift))
+        self.fshift = np.fft.fftshift(self.fft)
+        self.magnitudeSpec = 20*np.log(np.abs(self.fshift))
+        self.phaseSpec = 20*np.log(np.angle(self.fshift))
+        im = Image.fromarray(np.uint8(self.magnitudeSpec))
+        im.convert('RGB').save("FFT.jpg")
         
-    def idealLowpassFilter(self,frequency=100):
+    def idealLowpassFilter(self,frequency=10):
         filter = np.zeros((2*self.rows, 2*self.columns), dtype=np.int)
+        cropped = np.zeros((self.rows, self.columns), dtype = np.int)
+        
         a = self.rows / 2
         b = self.columns / 2
 
-        for row in range(self.rows):
-            for column in range(self.columns):
+        for row in range((-1*self.rows),self.rows):
+            for column in range((-1*self.columns),self.columns):
                 if ((row - a) ** 2 + (column - b) ** 2 <= frequency ** 2):
                     filter[row + a][column + b] = 1
+        im = Image.fromarray(np.uint8(filter))
+        im.convert('RGB').save("Filter.jpg")
+        outputFFT = np.multiply(self.fshift, filter)
+
+        im = Image.fromarray(np.uint8(20*np.log(np.abs(outputFFT))))
+        im.convert('RGB').save("IdealLPFFT.jpg")
+        
+        outputImage = np.fft.ifftshift(outputFFT)
+        outputImage = np.fft.ifft2(outputImage)
+        
+        for row in range(self.rows):
+            for column in range(self.columns):
+                cropped[row][column] = outputImage[row][column]
+        
+        im = Image.fromarray(np.uint8(cropped))
+        im.convert('RGB').save("IdealLP.jpg")
         
     
 
@@ -50,4 +70,4 @@ class fft_filter():
 if __name__ == "__main__":
     fft = fft_filter("lenna.jpg")
     #fft.graph()
-    fft.idealLowpassFilter()
+    fft.idealLowpassFilter(100)
